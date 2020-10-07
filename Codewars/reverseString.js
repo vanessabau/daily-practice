0fb70066f96afbbfca7396ad63492445e2840ebc
Vanessa Bautista <vbautista916@icloud.com>
2020-10-06 19:15:06 -0700

    additional solutions to reverse num

--- Codewars/reverseString.js
+++ Codewars/reverseString.js
@@ -13,3 +13,31 @@ function digitize(n) {
 
   return reverseNums;
 }
+
+////////Other solutions
+function digitize(n) {
+  return String(n).split("").map(Number).reverse();
+}
+
+//////////
+function digitize(n) {
+  return (n + "").split("").map(Number).reverse();
+}
+////////////
+function digitize(n) {
+  return Array.from(String(n), Number).reverse();
+}
+///////////
+function digitize(n) {
+  return String(n).split("").reverse().map(Number);
+}
+///////////
+function digitize(n) {
+  return n
+    .toString()
+    .split("")
+    .reverse()
+    .map(function (i) {
+      return parseInt(i);
+    });
+}
